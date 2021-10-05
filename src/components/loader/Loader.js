e03b84eb9b4d6d2dabaccd9b8021a4ad12997188
IgorSukovsky <Sukovsky70@gmail.com>
2021-10-05 08:14:51 +0300

    add hw-003

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Loader from "react-loader-spinner";
+import styles from "./Loader.module.css";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Loader
+        key="key"
+        className={styles.spinner}
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    );
+  }
+}
